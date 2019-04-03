4b98d221d245b25ff2900a09a2e2e96881ac45f7
wken01 <yuanxuemin-398@163.com>
2019-04-03 16:30:57 +0800

    二位

--- service-commons/src/main/java/com/hdx/common/Ca.java
+++ service-commons/src/main/java/com/hdx/common/Ca.java
@@ -6,6 +6,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * xxx
+ * @author Administrator
+ *
+ */
 @Target(ElementType.METHOD)   
 @Retention(RetentionPolicy.RUNTIME)    
 @Documented
